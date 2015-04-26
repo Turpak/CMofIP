4f2529e7acf2198aa0ab06c388ebf82ce9b0882e
Turpak <turpak_rom_15@mail.ru>
2015-04-26 22:58:41 +0700

    use pytest

--- Hash_algorithms../test_MD5.py
+++ Hash_algorithms../test_MD5.py
@@ -1,20 +1,23 @@
 import myMD5
 import hashlib
+import os
 
 def testMD51 ():
-    test_text = b"blablacar"
+    test_text = os.urandom(44)
     m = hashlib.md5(test_text).hexdigest()
     
     assert m == myMD5.hash_in_hex(myMD5.md5(test_text))
 
 def testMD52 ():
-    test_text = b""
+    test_text = os.urandom(99)
     m = hashlib.md5(test_text).hexdigest()
     
     assert m == myMD5.hash_in_hex(myMD5.md5(test_text))
 
 def testMD53 ():
-    test_text = b"my life, my rules"
+    test_text = os.urandom(222)
     m = hashlib.md5(test_text).hexdigest()
     
     assert m == myMD5.hash_in_hex(myMD5.md5(test_text))
+
+
