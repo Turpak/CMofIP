32592bf3ce68be3287a9387722dfa62c80c6d127
Turpak <turpak_rom_15@mail.ru>
2015-06-10 19:49:21 +0700

    Update test_RSA.py

--- Cipher_algorithms/myRSA/test_RSA.py
+++ Cipher_algorithms/myRSA/test_RSA.py
@@ -1,22 +1,12 @@
 import myRSA
 import bigNum
+import keygen
 
 def testRSA ():
-    with open('C:\RSA\encrypted_data.txt', 'rb') as file:
-        msg = file.read()
-    with open('C:\RSA\private_key.txt', 'rb') as f:
-        dd = f.readline()
-        nn = f.readline()
+    e, d, n = keygen.keygen() 
+    m = bigNum.random_big(n-10)
 
-    c = bigNum.bigNum(msg)
-    d = bigNum.bigNum(dd[:len(dd)-1])
-    n = bigNum.bigNum(nn)
-
-    m = myRSA.decrypt(c,d,n)
-    
-    with open('C:\RSA\data.txt', 'rb') as filename:
-        tmp = filename.read()
-
-    data = bigNum.bigNum(tmp[3:])
+    c = myRSA.encrypt(m,e,n)
+    data = myRSA.decrypt(c,d,n)
 
     assert m == data
